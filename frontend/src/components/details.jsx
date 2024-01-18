eda0406f60f5e9667c7a3f41fe7529f87bf553f8
Defreitasnicolas <defreitasnicolas@gmail.com>
2024-01-18 11:25:33 +0100

    page productDetails

---
+++ frontend/src/components/details.jsx
@@ -0,0 +1,16 @@
+import "../scss/components/Details.scss";
+
+function Details() {
+  return (
+    <div className="details">
+      <h2 className="brand">brand</h2>
+      <img className="picture" src="" alt="" />
+      <p className="description">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+        impedit nam quos vero facere ea fuga dolore maxime repellat rerum.
+      </p>
+    </div>
+  );
+}
+
+export default Details;

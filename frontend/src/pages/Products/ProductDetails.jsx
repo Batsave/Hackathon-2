4206de11b8b25f1cb7e1025e9ad7122218da9d48
Defreitasnicolas <defreitasnicolas@gmail.com>
2024-01-18 20:09:54 +0100

    page detailsProduct

--- frontend/src/pages/Products/ProductDetails.jsx
+++ frontend/src/pages/Products/ProductDetails.jsx
@@ -4,6 +4,25 @@ import Details from "../../components/details";
 import "../../scss/ProductDetails.scss";
 
 function ProductDetails() {
+  // const [productList, setProductList] = useState();
+  // useEffect(() => {
+  //   const getProductsList = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         `
+  //         ${import.meta.env.VITE_BACKEND_URL}/api/product/`,
+  //         {
+  //           withCredentials: true,
+  //         }
+  //       );
+  //       setProductList(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   getProductsList();
+  // }, []);
+
   // const [ProductsList, setproductList] = useState([]);
 
   // useEffect(() => {
@@ -17,8 +36,10 @@ function ProductDetails() {
   //       );
   //       setproductList(response.data);
   return (
-    <div className="detailsContainer">
-      <Details />
+    <div className="details_Container">
+      <div className="logo">
+        <Details />
+      </div>
     </div>
   );
 }
